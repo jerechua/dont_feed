b5275b29ddbafa199b3b7cf9436b7875c3649836
Jeremy <jere.chua@gmail.com>
2013-04-03 21:11:58 -0400

    setup for initial django stuffz

--- libs/database/models/Champions.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
-
-var championSchema = new Schema({
-
-});
-
-module.exports = mongoose.model('Champions', championSchema);
